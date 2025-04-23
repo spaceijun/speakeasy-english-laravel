f06159188432db989a0f1f87b8f7761ffd1bcc27
ijunnn <faizunaziz53@gmail.com>
2025-04-23 23:17:59 +0700

    AddRoutesLogoutApi

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,4 @@ Route::apiResource('grammars', GrammarController::class);
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
